9b45bc4146b420eb16100b129565704620b5e0f9
Cesar Lara <cesar.lara@yale.edu>
2020-05-21 07:33:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -116,9 +116,6 @@ def changing_alan
 end
 
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -135,4 +132,6 @@ def adding_to_dennis
       }
     }
 
+    programmer_hash[:dennis_ritchie][:languages] += "Assembly"
+    return programmer_hash
 end
